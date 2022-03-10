1f799133f755662f739fc8521b176182fc195ee3
Rifqinrfz <rifqifauzi085@gmail.com>
2022-03-10 18:34:21 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-undef */ /* eslint-disable no-unused-vars */
+/* eslint-disable eol-last */ /* eslint-disable indent */ /* eslint-disable linebreak-style */
+const notes = [];
+
+module.exports = notes;
---
+++ src/server.js
@@ -0,0 +1,25 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-undef */ /* eslint-disable no-unused-vars */
+/* eslint-disable eol-last */ /* eslint-disable indent */ /* eslint-disable linebreak-style */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    server.route(routes);
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
